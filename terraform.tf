d7d81f6dc152baae9c649957075e05f1b473bc42
sh940701 <visioner2168@gmail.com>
2024-03-23 22:25:16 +0900

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
+  /*
+    cloud {
+      organization = "organization-name"
 
-  cloud {
-    organization = "organization-name"
-
-    workspaces {
-      name = "learn-terraform-cloud"
+      workspaces {
+        name = "learn-terraform-cloud"
+      }
     }
-  }
+  */
 
   required_providers {
     aws = {
